6a27df291f82a5f2cfc9c49a595fbc8aaee0a825
LeshaKorzh <korzhlesha@gmail.com>
2017-06-26 15:27:42 +0300

    Change Swtich Clock and Calendar

--- clock-calendar/main.js
+++ clock-calendar/main.js
@@ -5,21 +5,20 @@ document.addEventListener('DOMContentLoaded', function () {
         calendar = document.querySelector('.calendar'),
         clock = document.querySelector('.clock');
 
+
     clockCalendar.addEventListener('mousedown', changeView);
 
     function changeView (event) {
+        debugger;
         if (event.button === 2) {
-            this.classList.toggle('switched');
-            if (this.classList.contains('switched')) {
-                switchBlocks.call(this, clockCalendar, calendar, clock);
-            } else {
-                switchBlocks.call(this, clockCalendar, clock, calendar);
+            if (calendar.classList.contains('disabled')) {
+                calendar.classList.remove('disabled');
+                clock.classList.add('disabled');
+            } else if (clock.classList.contains('disabled')) {
+                clock.classList.remove('disabled');
+                calendar.classList.add('disabled');
             }
 
         }
-    }
-
-    function switchBlocks (wrap, element1, element2) {
-        this.insertBefore(element1, this.firstChild);
     }
 });
