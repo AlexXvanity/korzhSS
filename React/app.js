1d715513bda3e2ee57303690093a6c5c388b7ed8
Oleksii Korzh <okorz@softserveinc.com>
2017-09-26 11:37:04 +0300

    change folder

--- React/app.js
+++
@@ -1,7 +0,0 @@
-var title = React.createElement('h1', null, 'Hello React');
-var img = React.createElement('img', { src:'https://facebook.github.io/react/img/logo.svg'});
-var subtitle = React.createElement('p', null, 'Библиотека для создания пользовательских интерфейсов');
-
-var container = React.createElement('div', {className:'container'}, title, img, subtitle);
-
-ReactDOM.render(container, document.getElementById('root'));
